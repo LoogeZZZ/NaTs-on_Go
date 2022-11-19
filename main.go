0bca3d7cf3c4e321df52e36db9626f16a3787669
Pavel Bezusov <looge.dark@mail.ru>
2022-11-19 10:44:22 +0300

    564645

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	go subToITH()
+
 	for {
 
 	}
@@ -16,21 +18,15 @@ func subToITH() {
 	// Connect to a server
 	nc, _ := nats.Connect("nats://95.165.107.100:4222")
 	defer nc.Drain()
+
 	// // Simple Publisher
 	// nc.Publish("oleja228 bot", []byte("Hello World"))
 
-	// Simple Async Subscriber
-	// nc.Subscribe(">", func(m *nats.Msg) {
-	// 	fmt.Printf("Received a message: %s\n", string(m.Data))
-	// })
-
 	// Responding to a request message
 	nc.Subscribe("ith", func(m *nats.Msg) {
-		m.Respond([]byte("Безусов принял, но не лсд)))"))
+		m.Respond([]byte("Безусов принял"))
 		fmt.Printf("Received a message: %s\n", string(m.Data))
 	})
-
-	nc.Subscribe("ith.*.students", func(m *nats.Msg) {})
 
 	// Close connection
 	// nc.Close()
